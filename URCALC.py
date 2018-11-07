c32706c3d46d48f29285977507269ab3c99e96d0
Harshawardhan Natu <harshawardhan.n333@gmail.com>
2018-11-07 17:46:43 +0530

    CodeChef - URCALC - Py3.6

---
+++ URCALC.py
@@ -0,0 +1,12 @@
+a = float(input())
+b = float(input())
+c = str(input())
+
+if(c == "+"):
+    print(a + b)
+elif(c == "-"):
+    print(a - b)
+elif(c == "*"):
+    print(a * b)
+elif(c == "/"):
+    print(a / b)
